def5d898afc078ed22ddd9fc91c324f5f25e674e
huber-dev <huber.lope@tecsup.edu.pe>
2025-09-12 22:08:26 -0500

    Solucion error

--- app/src/main/java/com/example/firstproject/components/layouts/ConflictoLab.kt
+++ app/src/main/java/com/example/firstproject/components/layouts/ConflictoLab.kt
@@ -8,4 +8,6 @@ import androidx.compose.runtime.Composable
 fun ConflictoLab() {
     //HL - modificado por Mercy Janampa
     Text(text = "Arreglando conflictos")
+    //Solucionando por Huber Lope
+    Text(text = "Conflicto resuelto")
 }
